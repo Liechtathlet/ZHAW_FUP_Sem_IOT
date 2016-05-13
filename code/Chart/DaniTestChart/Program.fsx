7a7bb521433b12782db12669ad9508739cb8f067
Liechtathlet <daniel.brun@gmx.net>
2016-05-13 10:30:03 +0200

    Update Charting

--- code/Chart/DaniTestChart/Program.fsx
+++ code/Chart/DaniTestChart/Program.fsx
@@ -1,13 +1,40 @@
-﻿#load "../packages/FSharp.Charting.Gtk.0.90.14/FSharp.Charting.Gtk.fsx"
+﻿#I "/usr/lib/cli/gtk-sharp-2.0/"
+#I "/usr/lib/cli/gdk-sharp-2.0/"
+#I "/usr/lib/cli/atk-sharp-2.0/"
+#I "/usr/lib/cli/glib-sharp-2.0/"
+#I "bin/Debug/"
 
-#r "/usr/lib/cli/gtk-sharp-2.0/gtk-sharp.dll"
-#r "./FSharp.Charting.Gtk.dll"
+#r "gtk-sharp.dll"
+#r "gdk-sharp.dll"
+#r "atk-sharp.dll"
+#r "glib-sharp.dll"
+#r "FSharp.Charting.Gtk.dll"
+
+#load "../packages/FSharp.Charting.Gtk.0.90.14/FSharp.Charting.Gtk.fsx"
 
 open FSharp.Charting
+open System
+open System.Threading
 
-Chart.Line([ for x in 0 .. 10 -> x, x*x ]).ShowChart()
 
+let runApp = async{
+    Chart.Line([ for x in 0 .. 10 -> x, x*x ]).ShowChart()
+    Gtk.Application.Run()
+    }
+
+let runMain  = async{
+
+    printfn "Starting application"
+    let! app = Async.StartChild runApp
+
+    printfn "Press any key to exit..."
+    let n = Console.ReadKey()
+    Gtk.Application.Quit()
+    printfn "Bye..."
+    }
+
+// run the whole workflow
+Async.RunSynchronously runMain  
 
 //1. xhost +localhost
-//2.  fsharpi --lib:./bin/Debug,/usr/lib/cli/gtk-sharp-2.0/,/usr/lib/cli/gdk-sharp-2.0/,/usr/lib/cli/atk-sharp-2.0/,/usr/lib/cli/glib-sharp-2.0/ 
-//3. #load "Program.fsx";;
+//2. fsharpi Program.fsx

b7aae06346f99beb9999abe6fc02d80af76b72d5
Liechtathlet <daniel.brun@gmx.net>
2016-05-19 22:35:23 +0200

    Sensor Data Conversion

--- code/GrovePiForDotNet/Raspberry.GrovePi/GrovePiWrapper.cs
+++ code/GrovePiForDotNet/Raspberry.GrovePi/GrovePiWrapper.cs
@@ -8,6 +8,7 @@ namespace Raspberry.GrovePi
 	//http://www.dexterindustries.com/GrovePi/engineering/port-description/
 	//http://www.dexterindustries.com/GrovePi/programming/grovepi-protocol-adding-custom-sensors/ 
 
+	//Firmware: https://github.com/DexterInd/GrovePi/blob/bfcaa57bb6ce2b5c4cb0057569ea38f3574f24cf/Firmware/Source/v1.2/grove_pi_v1_2_6/README.md
 	public class GrovePiWrapper
 	{
 		static readonly object _locker = new object();
@@ -32,12 +33,19 @@ namespace Raspberry.GrovePi
 				driver.Dispose();
 		}
 
+		/// <summary>
+		/// Reads the noise sensor data.
+		/// </summary>
+		/// <returns>The noise sensor data.</returns>
 		public int readNoiseSensorData(){
-			//Sensor-Pin: A0
+			// Spec: http://www.seeedstudio.com/wiki/Grove_-_Sound_Sensor
+			// Git: https://github.com/DexterInd/GrovePi/blob/57042c1e512dd973a10c07b2510beafccaf88f3f/Software/CSharp/GrovePi/Sensors/SoundSensor.cs
+
+			// Sensor-Pin: A0
 
 			byte[] ret;
 			lock (_locker) {
-				//Dummy, Action, Pin, Data, Data
+				//Dummy, Action: Read, Pin: 0, Unused, Unused
 				connection.Write (new[] { (byte)1, (byte)3, (byte)0, (byte)0, (byte)0 });
 				Thread.Sleep (10);
 
@@ -48,28 +56,46 @@ namespace Raspberry.GrovePi
 			return ((ret[1]*256) + ret[2]);
 		}
 
-		public int readTemperatureAndHumiditySensorData(){
-			//Sensor-Pin: A1
+		/// <summary>
+		/// Reads the temperature and humidity sensor data.
+		/// </summary>
+		/// <returns>The temperature and humidity sensor data. (Temperature, Humidity)</returns>
+		public Tuple<double,double> readTemperatureAndHumiditySensorData(){
+			// GIT: https://github.com/DexterInd/GrovePi/blob/1c16948fc3d747eada2ac326280e7d60bd0ebb25/Software/CSharp/GrovePi/Sensors/DHTTemperatureAndHumiditySensor%20.cs
+			// Spec: http://www.seeedstudio.com/wiki/Grove_-_Temperature_and_Humidity_Sensor
+
+			// Sensor-Pin: A1
 
 			byte[] ret;
 			lock (_locker) {
-				//Dummy, Action, Pin, Data, Data
-				connection.Write (new[] { (byte)1, (byte)3, (byte)1, (byte)0, (byte)0 });
-				Thread.Sleep (10);
+				//Action:40, Pin: 1, DHT-Model: 0, Unused
+				connection.Write (new[] { (byte)40, (byte)1, (byte)0, (byte)0 });
+				Thread.Sleep (600);
 
 				//Read sensor value (3 bytes, use the last 2)
-				ret = connection.Read (3);
+				ret = connection.Read (9);
 			}
 			//Convert to int
-			return ((ret[1]*256) + ret[2]);
+			//return ((ret[1]*256) + ret[2]);
+
+			double t = (double)BitConverter.ToSingle (ret, 1);
+			double h = (double)BitConverter.ToSingle (ret, 5);
+
+			return Tuple.Create (t, h);
 		}
-			
-		public int readLightSensorData(){
-			//Sensor-Pin: A2
+			/// <summary>
+			/// Reads the light sensor data.
+			/// </summary>
+			/// <returns>The light sensor data as raw value and light intensity / resistance. (sensorValue, resistance)</returns>
+		public Tuple<int,float> readLightSensorData(){
+			// GIT: https://github.com/DexterInd/GrovePi/blob/bfcaa57bb6ce2b5c4cb0057569ea38f3574f24cf/Software/CSharp/GrovePi/Sensors/LightSensor.cs
+			// Spec: http://www.seeedstudio.com/wiki/Grove_-_Light_Sensor
+
+			// Sensor-Pin: A2
 
 			byte[] ret;
 			lock (_locker) {
-				//Dummy, Action, Pin, Data, Data
+				//Dummy, Action:Read, Pin: 2, Unused, Unused
 				connection.Write (new[] { (byte)1, (byte)3, (byte)2, (byte)0, (byte)0 });
 				Thread.Sleep (10);
 
@@ -77,7 +103,11 @@ namespace Raspberry.GrovePi
 				ret = connection.Read (3);
 			}
 			//Convert to int
-			return ((ret[1]*256) + ret[2]);
+			//((ret[1]*256) + ret[2]);
+			int sensorValue =  BitConverter.ToInt32(ret,1);
+			float lightIntensity = (float)(1023-sensorValue)*10/sensorValue;
+
+			return Tuple.Create(sensorValue,lightIntensity);
 		}
 			
 		public void blink(int ms){

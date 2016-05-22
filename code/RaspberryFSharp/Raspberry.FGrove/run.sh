c721e7b7637303e02289816b18811148553f3167
Liechtathlet <daniel.brun@gmx.net>
2016-05-22 08:40:20 +0200

    Update doku, Kap 2

---
+++ code/RaspberryFSharp/Raspberry.FGrove/run.sh
@@ -0,0 +1,23 @@
+﻿#!/usr/bin/env bash
+
+trap_with_arg() { # from http://stackoverflow.com/a/2183063/804678
+  local func="$1"; shift
+  for sig in "$@"; do
+    trap "$func $sig" "$sig"
+  done
+}
+
+stop() {
+  trap - SIGINT EXIT
+  printf '\n%s\n' "recieved $1, killing children"
+  kill -s SIGINT 0
+}
+
+trap_with_arg 'stop' EXIT SIGINT SIGTERM SIGHUP
+
+(python "/home/pi/GrovePi/Software/Python/grove_led_blink.py") & sleep 3 ; kill $!
+
+cd "/home/pi/Development/FUP/Raspberry.FGrove/data"
+
+python -m SimpleHTTPServer 8000 &
+fsharpi --lib:/home/pi/Development/FUP/Raspberry.FGrove/libs "/home/pi/Development/FUP/Raspberry.FGrove/Program.fsx"
